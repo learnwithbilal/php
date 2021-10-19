fa3bd7bba31a63851df9a4624a0ca3a70bdab6ee
learnwithbilal <learn.with.bilal@gmail.com>
2021-10-19 16:47:32 +0300

    change extensions

--- index.php
+++ index.php
@@ -10,11 +10,12 @@
     <h1>PHP Course</h1>
     <ul>
       <a href="whyphp.html"><li>Why PHP?</li></a>
-      <a href="meaning.html"><li>Why PHP?</li></a>
+      <a href="meaning.html"><li>Meaning PHP?</li></a>
       <a href="whatyoulearn.html"><li>What You Learn</li></a>
-      <a href="xampp.html"><li>What You Learn</li></a>
+      <a href="xampp.html"><li>XAMPP</li></a>
       <a href="firstphp.html"><li>First PHP</li></a>
       <a href="arrays.html"><li>Arrays</li></a>
+      <a href="loops.html"><li>Loops</li></a>
     </ul>
   </body>
 </html>
---
+++ loops.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Loops</title>
+  </head>
+  <body>
+    <h1>Loops</h1>
+    <pre>
+    <code> for ($i=0 ; $i < 5; $i++){ 
+             echo 'some template';
+         }
+         </code>
+</pre>
+    <hr />
+    <pre>
+    <code> 
+        $blogs = ['blog01', 'blog02','blog03', 'blog04','blog05','blog06'];
+         </code>
+</pre>
+  </body>
+</html>
